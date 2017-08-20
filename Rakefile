98a68c257b973bb7265d1594a91416254b56bb86
Jeff Sember <jpsember@gmail.com>
2017-08-20 12:30:16 -0700

    Issue #21: additional experimental script to verify complement working

--- Rakefile
+++ Rakefile
@@ -8,5 +8,10 @@ end
 
 desc "Quick experiment"
 task :exp do
+  system("ruby lib/tokn/experiment.rb < test/exptokens.txt")
+end
+
+desc "Quick experiment #2"
+task :exp2 do
   system("ruby lib/tokn/experiment.rb < test/sampletokens.txt")
 end
--- lib/tokn/experiment.rb
+++ lib/tokn/experiment.rb
@@ -1,7 +1,24 @@
 require 'js_base'
 
 require_relative 'dfa'
+require_relative 'tokenizer'
+require_relative 'token'
+require_relative 'tokenizer_exception'
+
+text = FileUtils.read_text_file("test/exptext.txt")
+puts "Tokenizing:"
+puts text
+puts
 
 dfa = Tokn::DFA.from_script(ARGF.read)
 dfa.startState.generate_pdf("_SKIP_experiment.pdf")
+
+skipName = 'WS'
+
+tk =  Tokn::Tokenizer.new(dfa, text, skipName)
+
+while tk.has_next
+  t = tk.read
+  printf("%s %d %d %s\n",tk.name_of(t),t.lineNumber,t.column,t.text)
+end
 
