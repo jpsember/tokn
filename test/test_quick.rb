38f52d71bc083321d21d896df6065dadc47be52f
Jeff Sember <jpsember@gmail.com>
2017-08-29 12:51:01 -0700

    Issue #36: failing unit test

--- test/test_quick.rb
+++ test/test_quick.rb
@@ -6,6 +6,20 @@ class TestQuick < JSTest
   include ToknInternal
 
   def test_future
+    script =<<-'EOT'
+MISC: \d+
+ALPHA: abc
+BETA: abcdef
+EOT
+
+    begin
+      Tokn::DFACompiler.from_script(script)
+      raise "expected exception"
+    rescue ToknInternal::ParseException => e
+      assert e.message.include?("Redundant token")
+      assert e.message.include?("line 2;")
+    end
+
   end
 
 end
