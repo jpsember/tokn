bcf8532cd03506e812a4bfd183be9d0d7a8a6e9f
Jeff Sember <jpsember@gmail.com>
2017-09-09 11:45:59 -0700

    Issue #37

--- lib/tokn/code_set.rb
+++ lib/tokn/code_set.rb
@@ -276,15 +276,16 @@ module ToknInternal
 
     # Construct a CodeSet from a json representation
     #
-    def self.from_json(elements)
+    def self.from_json(json_representation)
 
       # If elements is not an array, wrap it as an array with a single element
-      if !(elements.is_a? Array)
-        elements = [elements]
+      if !(json_representation.is_a? Array)
+        elements = [json_representation]
+      else
+        elements = json_representation
       end
 
       code_set = CodeSet.new
-      result = code_set.elements
 
       cursor = 0
       while cursor < elements.length
@@ -300,7 +301,7 @@ module ToknInternal
           a = a.to_i
           b = a + 1
         end
-        result << a << b
+        code_set.elements << a << b
       end
 
       code_set
