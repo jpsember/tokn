07b647c2abfe7098892b837217c4cd950fff0d70
Jeff Sember <jpsember@gmail.com>
2017-09-09 12:09:43 -0700

    Issue #37: renaming file

--- lib/tokn/compiler.rb
+++ lib/tokn/compiler.rb
@@ -4,7 +4,7 @@ require_relative "../tokn"
 
 require_relative "compiler/dfa_compiler"
 require_relative "compiler/reg_parse"
-require_relative "compiler/dfa_builder"
+require_relative "compiler/nfa_to_dfa"
 require_relative "compiler/range_partition"
 require_relative "compiler/dfa_filter"
 require_relative "compiler/dfa_compiler"
