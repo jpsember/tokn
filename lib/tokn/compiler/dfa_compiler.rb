9fe7cba7f636d201eb624543b87e41758a4689ef
Jeff Sember <jpsember@gmail.com>
2017-09-09 11:58:59 -0700

    Issue #37: refactoring

--- lib/tokn/compiler/dfa_compiler.rb
+++ lib/tokn/compiler/dfa_compiler.rb
@@ -24,67 +24,9 @@ module Tokn
       td.parse(script)
     end
 
-    # Serialize DFA to a JSON string.
-    # The DFA in JSON form has this structure:
-    #
-    #  {
-    #    "version" => version number (float)
-    #    "tokens" => array of token names (strings)
-    #    "states" => array of states, ordered by id (0,1,..)
-    #    "final" => id of final state
-    #  }
-    #
-    # Each state is a list of edges:
-    #  [edge0, edge1, ...]
-    #
-    # Each edge is a list:
-    #  [label, destination id]
-    #
-    # Labels are arrays of integers, exactly the structure of
-    # a CodeSet array.
-    #
-    def self.serialize(dfa)
-
-      require "json"
-
-      dict = {"version" => DFA.version, "tokens" => dfa.token_names}
-
-      state_info = []
-
-      state_list = self.get_ordered_state_list(dfa)
-      state_list.each do |state|
-        if state.final_state
-          raise "multiple final states" if dict.include? "final"
-          dict["final"] = state.id
-        end
-
-        edge_list = []
-        state.edges.each do |lbl, dest|
-          edge_list << lbl.to_json
-          edge_list << dest.id
-        end
-        state_info << edge_list
-      end
-      dict["states"] = state_info
-
-      JSON.generate(dict)
-    end
-
 
     private
 
-
-    def self.get_ordered_state_list(dfa)
-      raise ArgumentError, "Bad start state" if dfa.start_state.id != 0
-      state_list = []
-      id_to_state_map = {}
-      dfa.start_state.reachable_states.each{|st| id_to_state_map[st.id] = st}
-      id_to_state_map.size.times do |id|
-        state = id_to_state_map[id] or raise ArgumentError, "unexpected state ids"
-        state_list << state
-      end
-      state_list
-    end
 
   end
 
--- lib/tokn/dfa.rb
+++ lib/tokn/dfa.rb
@@ -1,3 +1,5 @@
+require "json"
+
 module Tokn
 
   # A DFA for tokenizing; includes pointer to a start state, and
@@ -11,10 +13,56 @@ module Tokn
 
     include ToknInternal
 
+    # Serialize DFA to a JSON string.
+    # The DFA in JSON form has this structure:
+    #
+    #  {
+    #    "version" => version number (float)
+    #    "tokens" => array of token names (strings)
+    #    "states" => array of states, ordered by id (0,1,..)
+    #    "final" => id of final state
+    #  }
+    #
+    # Each state is a list of edges:
+    #  [edge0, edge1, ...]
+    #
+    # Each edge is a list:
+    #  [label, destination id]
+    #
+    # Labels are arrays of integers, exactly the structure of
+    # a CodeSet array.
+    #
+    def to_json
+
+      dict = {}
+      dict["version"] = DFA.version
+      dict["tokens"] = @token_names
+
+      state_info = []
+
+      state_list = get_ordered_state_list
+      state_list.each do |state|
+        if state.final_state
+          raise "multiple final states" if dict.include? "final"
+          dict["final"] = state.id
+        end
+
+        edge_list = []
+        state.edges.each do |lbl, dest|
+          edge_list << lbl.to_json
+          edge_list << dest.id
+        end
+        state_info << edge_list
+      end
+      dict["states"] = state_info
+
+      JSON.generate(dict)
+    end
+
+
     # Compile a Tokenizer DFA from a JSON string
     #
     def self.from_json(json_str)
-      require "json"
       h = JSON.parse(json_str)
       version = h["version"]
 
@@ -95,6 +143,18 @@ module Tokn
 
     private
 
+
+    def get_ordered_state_list
+      raise ArgumentError, "Bad start state" if @start_state.id != 0
+      state_list = []
+      id_to_state_map = {}
+      @start_state.reachable_states.each{|st| id_to_state_map[st.id] = st}
+      id_to_state_map.size.times do |id|
+        state = id_to_state_map[id] or raise ArgumentError, "unexpected state ids"
+        state_list << state
+      end
+      state_list
+    end
 
   end
 
